dcf63d3e2e1d59ed77646d49ceb2d9fae746f508
Oliver Broadrick <odbroadrick@gmail.com>
2022-09-09 17:46:38 -0400

    button

--- src/Interface.java
+++ src/Interface.java
@@ -6,17 +6,35 @@ import javax.swing.*;
 public class Interface extends JFrame {
     public Interface() {
         // Make a graphical user interface frame for this interface
-        this.setTitle("LOR Simulator")
+        this.setTitle("LOR Simulator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000,1000);
-        this.setVisible(true);
+        int w = 800;
+        int h = 600;
+        this.setSize(w,h);
+
+        // Testing checkbox
+        JRadioButton button = new JRadioButton();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello, world!");
+            }
+        });
+        this.add(button);
 
         // Create the computer simulator instance for this interface
         int size = 2048;
         Simulator S = new Simulator(size);
+
+        // Set visibile
+        this.setVisible(true);
     }
     /* 
     JButton button = new JButton("Press");
     this.getContentPane().add(button);
     */
 }
+
+/**
+ * A way to visualize a register
+ */
