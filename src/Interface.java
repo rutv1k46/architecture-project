bf1d12071f59a99f3bb56f1251813f0235935990
Oliver Broadrick <odbroadrick@gmail.com>
2022-09-10 18:48:40 -0400

    more

--- src/Interface.java
+++ src/Interface.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
-
+import java.awt.event.*;
+import java.util.Arrays;
 /**
  * A graphical user interface for using the simulated computer
  */
 public class Interface extends JFrame {
+    JPanel panel;
+    Simulator S;
+
     public Interface() {
         // Make a graphical user interface frame for this interface
         this.setTitle("LOR Simulator");
@@ -11,30 +15,67 @@ public class Interface extends JFrame {
         int w = 800;
         int h = 600;
         this.setSize(w,h);
-
-        // Testing checkbox
-        JRadioButton button = new JRadioButton();
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Hello, world!");
-            }
-        });
-        this.add(button);
+        this.panel = new JPanel();
+        this.add(this.panel);
 
         // Create the computer simulator instance for this interface
         int size = 2048;
-        Simulator S = new Simulator(size);
+        this.S = new Simulator(size);
+
+        // Add registers
+        addRegisters();
 
         // Set visibile
         this.setVisible(true);
     }
-    /* 
-    JButton button = new JButton("Press");
-    this.getContentPane().add(button);
-    */
+
+    public void addRegisters() {
+        String[] reg_names = {"R0", "R1", "R2", "R3", "PC", "CC", "IR", "MAR", "MBR", "X1", "X2", "X3"};
+        int[] reg_sizes = {16, 16, 16, 16, 16, 16, 16, 16.......};
+        for (String reg_name : reg_names) {
+            Register R1 = new Register(this.S, this.panel, reg_name, 16);
+        }
+    }
 }
 
-/**
- * A way to visualize a register
- */
+class Register {
+    JRadioButton[] buttons;
+    int[] arr;
+    String name;
+    public Register(Simulator S, JPanel panel, String name, int size) {
+        this.name = name;
+        buttons = new JRadioButton[size];
+        arr = new int[size];
+        Box box = new Box(BoxLayout.X_AXIS);
+        for (int i = 0; i < size; i++) {
+            buttons[i] = new JRadioButton();
+            buttons[i].addActionListener(new RegisterListener(S, this, i));
+            box.add(buttons[i]);
+        }
+        panel.add(box);
+    }
+}
+
+class RegisterListener implements ActionListener {
+    Simulator S;
+    Register R;
+    int i;
+    public RegisterListener(Simulator S, Register R, int i) {
+        this.S = S;
+        this.R = R;
+        this.i = i;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Update this register's value in the register object
+        if (this.R.arr[i] == 0) {
+            this.R.arr[i] = 1;
+        } else {
+            this.R.arr[i] = 0;
+        }
+        // Update this register's value in the simulator
+        S.set(this.R.name, this.R.arr);
+        // For debugging: print updated value of this register
+        System.out.println(Arrays.toString(this.R.arr));
+    }
+}
--- src/Simulator.java
+++ src/Simulator.java
@@ -60,5 +60,51 @@ public class Simulator {
         this.X3 = new int[16];
         this.M = new Memory(size);
     }
+
+    /**
+     * Sets the register with name n to have the value v.
+     */
+    public void set(String name, int[] v) {
+        switch (name) {
+            case "R0":
+                for (int i=0; i<this.R0.length; i++) this.R0[i] = v[i];
+                break;
+            case "R1":
+                for (int i=0; i<this.R0.length; i++) this.R1[i] = v[i];
+                break;
+            case "R2":
+                for (int i=0; i<this.R0.length; i++) this.R2[i] = v[i];
+                break;
+            case "R3":
+                for (int i=0; i<this.R0.length; i++) this.R3[i] = v[i];
+                break;
+            case "PC":
+                for (int i=0; i<this.R0.length; i++) this.PC[i] = v[i];
+                break;
+            case "CC":
+                for (int i=0; i<this.R0.length; i++) this.CC[i] = v[i];
+                break;
+            case "IR":
+                for (int i=0; i<this.R0.length; i++) this.IR[i] = v[i];
+                break;
+            case "MAR":
+                for (int i=0; i<this.R0.length; i++) this.MAR[i] = v[i];
+                break;
+            case "MBR":
+                for (int i=0; i<this.R0.length; i++) this.MBR[i] = v[i];
+                break;
+            case "X1":
+                for (int i=0; i<this.R0.length; i++) this.X1[i] = v[i];
+                break;
+            case "X2":
+                for (int i=0; i<this.R0.length; i++) this.X2[i] = v[i];
+                break;
+            case "X3":
+                for (int i=0; i<this.R0.length; i++) this.X3[i] = v[i];
+                break;
+            default:
+                System.out.println("In the set function in the Simulator class recieved string: "+name);
+        }
+    }
     
 }
